4ea6980e3f7db6d0b8afcf0b2e916ccc72b1c63b
Ednilton Junior <jrjunior_cg@live.com>
2020-10-08 22:12:07 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const venom = require('venom-bot');
+const express = require('express');
+const app = express();
+
+app.listen('5005');
+
+  venom.create().then((client) => start(client));
+
+  function start(client) {
+    client.onMessage((message) => {
+      if (message.body === 'Hi' && message.isGroupMsg === false) {
+        client
+          .sendText(message.from, 'Welcome Venom 🕷')
+          .then((result) => {
+            console.log('Result: ', result); //return object success
+          })
+          .catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+          });
+      }
+    });
+  }
+
+
+
