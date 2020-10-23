ec883bcb40e18f59d9310d0b25d6adaefbffe22d
carloselmartecnologia <carloselmartecnologia@gmail.com>
2020-10-23 11:08:31 -0300

    change session controller

--- src/app/controllers/SessionController.ts
+++ src/app/controllers/SessionController.ts
@@ -4,6 +4,10 @@ class SessionController {
   async login(req: Request, res: Response) {
     res.json('login realizado com sucesso');
   }
+
+  async logout(req: Request, res: Response) {
+    res.json("logout realizado com sucesso");
+  }
 }
 
 export default new SessionController();
