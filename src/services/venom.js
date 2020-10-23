d5e76ba1ac29580542b6618c167bb07202eaecfd
carloselmartecnologia <carloselmartecnologia@gmail.com>
2020-10-22 22:13:55 -0300

    teste

--- src/services/venom.js
+++ src/services/venom.js
@@ -1,19 +1,16 @@
 const venom = require('venom-bot');
-  venom.create().then((client) => start(client));
-  function start(client) {
-    client.onMessage((message) => {
-      if (message.body === 'Hi' && message.isGroupMsg === false) {
-        client
-          .sendText(message.from, 'Welcome Venom 🕷')
-          .then((result) => {
-            console.log('Result: ', result); //return object success
-          })
-          .catch((erro) => {
-            console.error('Error when sending: ', erro); //return object error
-          });
-      }
-    });
-  }
-
-
-
+venom.create().then((client) => start(client));
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body === 'Hi' && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Welcome Venom 🕷')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
+  });
+}
